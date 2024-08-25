71cc9ea69a59136b578fb671a8816c486a46e2e0
Jamile Lenharo <mile_lenharo@hotmail.com>
2024-08-25 14:35:09 -0300

    Conserto página inicial

--- js/script_index.js
+++ js/script_index.js
@@ -1,4 +1,33 @@
 function loadContent(page) {
+
+    if (page === "index.html") {
+        // Limpa o conteúdo da seção para evitar duplicação
+        document.getElementById('content-section').innerHTML = `
+            <b><h2>O que são as tags em HTML?</h2></b>
+            <img src="Imagens/Imagem home.png" alt="Imagem exemplo de uma tabela com algumas tags utilizadas em html" width="249" height="221">
+            <p>Elas são usadas para informar ao navegador a estrutura do site. Através delas é que o navegador irá interpretar o que é a estrutura e o conteúdo visual da sua página. Existem diferentes tipos de tags, cada um com suas particularidades e funções. Podemos citar:</p>
+            <ol>
+                <b>
+                    <li>Estruturais</li>
+                    <li>Título</li>
+                    <li>Texto</li>
+                    <li>Link</li>
+                    <li>Multimídia</li>
+                    <li>Lista</li>
+                    <li>Estilos e Script</li>  
+                </b>
+            </ol>
+            <p>Para saber mais sobre cada tipo de tag, utilize a barra de navegação acima.</p>
+        `;
+        return; // Encerra a função para evitar o carregamento do conteúdo duplicado
+    }
+
+    // Remove qualquer CSS existente que tenha sido carregado dinamicamente
+    const existingLink = document.getElementById('dynamic-css');
+    if (existingLink) {
+        existingLink.remove();
+    }
+
     fetch(page)
         .then(response => response.text())
         .then(data => {
